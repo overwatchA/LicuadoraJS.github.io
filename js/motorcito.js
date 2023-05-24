e974c127405d20b76773502775cc5d882b26a689
overwatchA <125980409+overwatchA@users.noreply.github.com>
2023-05-24 17:59:46 -0500

    Add files via upload

---
+++ js/motorcito.js
@@ -0,0 +1,33 @@
+var estadoLicuadora = "apagada";
+var sonidoLicuadora = document.getElementById("blendersound");
+var botonLicuadora = document.getElementById("blender-button-sound");
+var licuadora = document.getElementById("blender");
+
+
+function controlarLicuadora(){
+  if (estadoLicuadora == "apagada"){
+    estadoLicuadora = "encendido";
+    hacerBrrBrr();
+    licuadora.classList.add("active")
+    // console.log("me prendiste");
+  } else {
+    estadoLicuadora = "apagada";
+    hacerBrrBrr();
+    licuadora.classList.remove("active")
+    // console.log("me apagaste");
+  }
+}
+
+//.paused es una propiedad de js que me devuelve .paused si está en pausa y false si se está reproduciendo 
+
+function hacerBrrBrr(){
+  if (sonidoLicuadora.paused) {
+    botonLicuadora.play();
+    sonidoLicuadora.play();
+  } else {
+    botonLicuadora.play();
+    sonidoLicuadora.pause();
+    sonidoLicuadora.currentTime = 0;
+
+  }
+}
